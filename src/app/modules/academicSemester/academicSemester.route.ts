174a0d3159dc66822862ae8274dced0f1e3be71f
creativerze <creativerze@gmail.com>
2023-08-20 23:34:08 +0600

    added get academicSemester

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.get('/', AcademicSemesterController.getAllFromDB);
+
+router.get('/:id', AcademicSemesterController.getDataById);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
 
 export const AcademicSemesterRoutes = router;
