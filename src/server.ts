fb17e08cbdf0fefa9c976080d4c786001b291c22
creativerze <creativerze@gmail.com>
2023-10-05 19:44:38 +0600

    update code

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import { Server } from 'http';
 import app from './app';
+import subscribeToEvents from './app/events';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
 import { RedisClient } from './shared/redis';
 
-async function bootstrap() {
-  await RedisClient.connect();
 
+async function bootstrap() {
+  await RedisClient.connect().then(() => {
+    subscribeToEvents()
+  });
   const server: Server = app.listen(config.port, () => {
     logger.info(`Server running on port ${config.port}`);
   });
 
   const exitHandler = () => {
+
     if (server) {
       server.close(() => {
         logger.info('Server closed');
@@ -28,12 +32,12 @@ async function bootstrap() {
   process.on('uncaughtException', unexpectedErrorHandler);
   process.on('unhandledRejection', unexpectedErrorHandler);
 
-  process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
-    if (server) {
-      server.close();
-    }
-  });
+  // process.on('SIGTERM', () => {
+  //   logger.info('SIGTERM received');
+  //   if (server) {
+  //     server.close();
+  //   }
+  // });
 }
 
 bootstrap();
